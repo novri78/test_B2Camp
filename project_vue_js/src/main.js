18666c1f28894c193a2627f330a9bbe6dc402714
Novri Marizal <novri.marizal24@gmail.com>
2024-05-23 14:47:40 +0700

    create project_vue_js

---
+++ project_vue_js/src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+//import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import {createBootstrap} from 'bootstrap-vue-next'
+
+// Add the necessary CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import './assets/css/style.css'
+import './assets/scss/_style.scss'
+
+const app = createApp(App).use(store).use(router)
+app.use(createBootstrap({components: true, directives: true})) // Change this line
+app.mount('#app')
---
+++ project_vue_js/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="..//assets/img/logo.png" />
+    <HelloWorld msg="Welcome B2Camp Members into Your Vue.js App" />
+    <BCarousel controls>
+      <BCarouselSlide img-src="https://picsum.photos/1024/480/?image=1" />
+      <BCarouselSlide img-src="https://picsum.photos/1024/480/?image=2" />
+      <BCarouselSlide img-src="https://picsum.photos/1024/480/?image=3" />
+    </BCarousel>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
